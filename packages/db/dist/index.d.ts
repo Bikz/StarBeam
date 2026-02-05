5d7b00deeef82c3bf605059f3ff7d1c7ebc37264
Bikz <bikrambrar25@gmail.com>
2026-02-05 13:18:33 -0800

    chore: ignore build outputs

--- packages/db/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-declare global {
-    var __starbeam_prisma__: PrismaClient | undefined;
-}
-declare const prisma: PrismaClient;
-
-export { prisma };
